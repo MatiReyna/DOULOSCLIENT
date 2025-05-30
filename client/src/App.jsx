a4473a3d7dbe20f5551bd111acb291f52220ac02
Mati Reyna <matiireyna@hotmail.com>
2025-05-29 23:51:50 -0300

    Ultima revision antes de iniciar el proyecto

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div className='min-h-screen flex items-center justify-center bg-blue-600 text-white'>
-      <h1 className='text-4xl font-bold'>¡Tailwind está funcionando! 🚀</h1>
+      <h1 className='text-4xl font-semibold'>¡Tailwind está funcionando! 🚀</h1>
     </div>
   )
 }
